7296e51340770b8e3c76435ebe5e2dbd98b29553
Mahdi <mahdih2105@gmail.com>
2024-08-21 01:06:39 +0100

    trying functions and loops

--- basics.py
+++ basics.py
@@ -12,3 +12,49 @@ def add2(num):
 
 print(rn.randint(1, 10))
 print(randint(10,20))
+
+# fruit = input('Please enter a fruit')
+
+# if fruit == 'apple':
+#     print('the fruit is: ' + fruit)
+# else: 
+#     print('You did not enter apple, but you entered: ' + fruit)
+
+
+one_to_ten = randint(1, 10)
+ 
+if one_to_ten == 1:
+    print("The roman numeral equivalent of " + str(one_to_ten) + " is I.")
+elif one_to_ten == 2:
+    print("The roman numeral equivalent of " + str(one_to_ten) + " is II.")
+elif one_to_ten == 3:
+    print("The roman numeral equivalent of " + str(one_to_ten) + " is III.")
+elif one_to_ten == 4:
+    print("The roman numeral equivalent of " + str(one_to_ten) + " is IV.")
+elif one_to_ten == 5:
+    print("The roman numeral equivalent of " + str(one_to_ten) + " is V.")
+elif one_to_ten == 6:
+    print("The roman numeral equivalent of " + str(one_to_ten) + " is VI.")
+elif one_to_ten == 7:
+    print("The roman numeral equivalent of " + str(one_to_ten) + " is VII.")
+elif one_to_ten == 8:
+    print("The roman numeral equivalent of " + str(one_to_ten) + " is VIII.")
+elif one_to_ten == 9:
+    print("The roman numeral equivalent of " + str(one_to_ten) + " is IX.")
+else:
+    print("The roman numeral equivalent of " + str(one_to_ten) + " is X.")
+
+
+for num in range(1, 51): # 3rd argument can be added for step i.e range(1, 5, 3) goes up in 3s
+    # If num is divisible by both 3 and 5, "FizzBuzz" will be printed.
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    # If num is only divisible by 3, "Fizz" will be printed.
+    elif num % 3 == 0:
+        print("Fizz")
+    # If num is only divisible by 5, "Buzz" will be printed.
+    elif num % 5 == 0:
+        print("Buzz")
+    # num itself will be printed in all other cases.
+    else:
+        print(num)
