03fc3ae89585f43fef0b828763a35c364022424f
Mahdi <mahdih2105@gmail.com>
2024-08-21 22:46:42 +0100

    working with lists

--- basics.py
+++ basics.py
@@ -45,6 +45,8 @@ else:
     print("The roman numeral equivalent of " + str(one_to_ten) + " is X.")
 
 
+# FIZZBUZZ TASK
+
 for num in range(1, 51): # 3rd argument can be added for step i.e range(1, 5, 3) goes up in 3s
     # If num is divisible by both 3 and 5, "FizzBuzz" will be printed.
     if num % 3 == 0 and num % 5 == 0:
@@ -58,3 +60,83 @@ for num in range(1, 51): # 3rd argument can be added for step i.e range(1, 5, 3)
     # num itself will be printed in all other cases.
     else:
         print(num)
+
+the_string = "North Dakota"
+print(the_string.rjust(17))  
+print(the_string.ljust(17, "*"))  
+center_plus = the_string.center(16, "+")  
+print(center_plus)
+print(the_string.lstrip("North"))
+print(center_plus.rstrip("+"))  
+print(center_plus.strip("+"))  
+print(the_string.replace("North", "South"))  
+
+user_string = "reverse this"
+reversed = ""
+ 
+for item in range(len(user_string) - 1, -1, -1): # Starts at the last index,
+                                                 # Stops at index before -1 (0, which is first index)
+                                                 # Step by -1, goes backwards in index
+    reversed += user_string[item]
+ 
+print(reversed)
+
+
+str_1 = "James Bond is 007."
+str_2 = "When the moon hits your eye like a big pizza pie, that's amore!"
+str_3 = "Anyway, like I was sayin', shrimp is the fruit of the sea. You can barbecue it, boil it, broil it, bake it, \
+saute it. Dey's uh, shrimp-kabobs, shrimp creole, shrimp gumbo. Pan fried, deep fried, stir-fried. There's pineapple \
+shrimp, lemon shrimp, coconut shrimp, pepper shrimp, shrimp soup, shrimp stew, shrimp salad, shrimp and potatoes, \
+shrimp burger, shrimp sandwich. That- that's about it."
+ 
+# This function reduces the string to letters, numbers, spaces, hyphens, and apostrophes, 
+# and assigns that string to the variable spaces_and_letters so that the number of words 
+# in it can by found by counting spaces between words.
+def word_counter(words):
+    spaces_and_letters = ""
+    word_count = 1
+    for i in words:
+        if i.isalnum() or i.isspace() or i == "-" or i == "'":
+            spaces_and_letters += i
+    for j in spaces_and_letters:
+        if j == " ":
+            word_count += 1
+    return word_count
+ 
+ 
+print(word_counter(str_1))
+print(word_counter(str_2))
+print(word_counter(str_3))
+
+name = 'Mahdi'
+age = 23
+
+print("{} is {}".format(name, age))
+
+# LISTS AND LIST METHODS
+
+mixed = [10, 4.97, True, "mountain", [9, 8, 7]]
+li_str = list("cheese")
+print("e" in li_str)
+print("a" not in li_str)
+
+up_by_two = [[0, 2], [4, 6], [8, 10], [12, 14]]
+print(up_by_two[0])
+print(up_by_two[3][1])
+furniture = ["chair", "table", "desk", "lamp", "bed"]
+print(furniture[-5])
+print("Most people own at least " + str(up_by_two[0][1]) + " " + furniture[0] + "s.")
+floats = [0.98, 8.76, 6.54, 4.32]
+print(floats[1:])
+print(floats[1:3])
+print(floats[:2])
+
+arctic_animals = ["penguin", "elephant", "polar bear", "walrus", "tiger", "reindeer"]
+del arctic_animals[4]
+arctic_animals.remove("elephant")
+arctic_animals.append("arctic fox")
+arctic_animals.insert(2, "snowy owl")  
+arctic_animals.sort()  
+print(arctic_animals)
+print(arctic_animals.index("reindeer"))
+print(arctic_animals.pop())
